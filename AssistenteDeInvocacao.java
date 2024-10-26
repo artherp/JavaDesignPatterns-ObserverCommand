0c0397d23da20999a52bd3f5e995b1e01cd7d3f5
Arthur Erpen <77343288+artherp@users.noreply.github.com>
2024-10-26 12:27:28 -0300

    Changes in main and in the invoker

--- AssistenteDeInvocacao.java
+++ AssistenteDeInvocacao.java
@@ -1,9 +1,5 @@
 public class AssistenteDeInvocacao implements Observer {
     
-    public void executarComando(Servico instrucao) {
-        instrucao.execute();
-    }
-    
     @Override
     public void update(Observable o, Object arg) {
         System.out.println("O dispositivo foi atualizado"); /* não modifique */
--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
+
+import java.util.ArrayList;
+
 /*
 *  Se você está realizando esta avaliação em dupla, 
 *  coloque o nome do outro aluno (que não está logado no Moodle) abaixo.
 *  Se você está realizando esta avaliação sozinho, não preencha o nome abaixo.
 *  Nome completo: 
-*/
-
-
+ */
 public class Main {
-     public static void main(String[] args) {
-        
+
+    public static void main(String[] args) {
+
         // Você deve criar uma televisão, uma caixa de som e uma lâmpada RGB
-        /* complete */ 
+        /* complete */
         CaixaDeSom caixa = new CaixaDeSom();
         Televisao tv = new Televisao();
         LampadaRGB lamp = new LampadaRGB();
@@ -18,7 +20,7 @@ public class Main {
         // Cria serviços (desvinculados das classes que executam os serviços e das classes que invoccam os serviços)
         // Você deve criar instâncias com cada um dos serviços de cada um dos dispositivos
         /* complete */
-        Servico  ligueTV = new ServicoLigue(tv);
+        Servico ligueTV = new ServicoLigue(tv);
         Servico ligueCaixa = new ServicoLigue(caixa);
         Servico ligueLampadaRGB = new ServicoLigue(lamp);
         Servico ajusteVolumeCaixaSom = new ServicoAjustePotencia(caixa);
@@ -29,16 +31,27 @@ public class Main {
         Servico desligueTelevisao = new ServicoDesligue(tv);
         Servico desligueCaixaDeSom = new ServicoDesligue(caixa);
         Servico desligueLampadaRGB = new ServicoDesligue(lamp);
-        
-
 
         // Cria a alexa (invocador dos serviços, desvinculado dos serviços e de quem executa os serviços)
         // Cria um assiste de invocação e registre esse assistente como observador de cada um dos dispositivos
+        ArrayList<Servico> alexa = new ArrayList<>();
         
-        AssistenteDeInvocacao alexa = new AssistenteDeInvocacao();
-        tv.addObserver(alexa);
-        caixa.addObserver(alexa);
-        lamp.addObserver(alexa);
+        AssistenteDeInvocacao assistente = new AssistenteDeInvocacao();
+        tv.addObserver(assistente);
+        caixa.addObserver(assistente);
+        lamp.addObserver(assistente);
+        
+        alexa.add(ligueTV);
+        alexa.add(ligueCaixa);
+        alexa.add(ligueLampadaRGB);
+        alexa.add(ajusteVolumeCaixaSom);
+        alexa.add(ajusteVolumeTelevisao);
+        alexa.add(ajusteCanalTelevisao);
+        alexa.add(ajusteBrilhoLampada);
+        alexa.add(ajusteCorLampada);
+        alexa.add(desligueTelevisao);
+        alexa.add(desligueCaixaDeSom);
+        alexa.add(desligueLampadaRGB);
 
         // Invocador dispara serviços para serem executados pelos receptores dos serviços)
         // Você deve fazer com que o assistente execute cada um dos serviços em cada um dos dispositivos,
@@ -46,18 +59,13 @@ public class Main {
         // ajusteVolumeTelevisao, ajusteCanalTelevisao, ajusteBrilhoLampada, ajusteCorLampada, 
         // desligueTelevisao, desligueCaixaDeSom, desligueLampadaRGB
         /* complete */
-        
-        alexa.executarComando(ligueTV);
-        alexa.executarComando(ligueCaixa);
-        alexa.executarComando(ligueLampadaRGB);
-        alexa.executarComando(ajusteVolumeCaixaSom);
-        alexa.executarComando(ajusteVolumeTelevisao);
-        alexa.executarComando(ajusteCanalTelevisao);
-        alexa.executarComando(ajusteBrilhoLampada);
-        alexa.executarComando(ajusteCorLampada);
-        alexa.executarComando(desligueTelevisao);
-        alexa.executarComando(desligueCaixaDeSom);
-        alexa.executarComando(desligueLampadaRGB);
-        
+        alexaExecuter(alexa);
+
+    }
+
+    public static void alexaExecuter(ArrayList<Servico> alexaCommands) {
+        for (Servico comando : alexaCommands) {
+            comando.execute();
         }
-     }
+    }
+}
